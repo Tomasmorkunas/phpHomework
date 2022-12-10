2bd1acf4aedb1edc2f8c72a08ec778af67a7bf07
Tomas <tomas.morkunas@stud.vilniustech.lt>
2022-12-10 02:36:28 +0200

    login design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,34 +1,37 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" style="height: 100%">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
 </head>
 <body class="login-body">
 <form action="{{ route('login') }}" method="post">
     @csrf
     <div>
-        <label for="username-input">Username</label>
-        <input type="text" id="username-input" name="username" value="{{old('username')}}">
+        <label for="username-input" class="login-label">Username</label>
+        <input type="text" id="username-input" class="login-input" name="username" value="{{old('username')}}">
         @error('username')
         <p>{{$message}}</p>
         @enderror
     </div>
     <div>
-        <label for="password-input">Password</label>
-        <input type="password" id="password-input" name="password" value="{{old('password')}}">
+        <label for="password-input" class="login-label">Password</label>
+        <input type="password" id="password-input" class="login-input" name="password" value="{{old('password')}}">
         @error('password')
         <p>{{$message}}</p>
         @enderror
     </div>
     <div>
-        <label for="remember-input">Remember me</label>
+        <label for="remember-input" class="login-label ">Remember me</label>
         <input type="checkbox" id="remember-input" name="remember" value="1">
     </div>
-    <input type="submit" value="Login">
+    <input type="submit" class="btn btn-warning mygtuko-apvalinimas mt-3" value="Login">
+
 </form>
 </body>
 </html>
